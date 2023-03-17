30371c720c498e31876686f0604aa5c1e523c76e
Your Name <roger1030142@gmail.com>
2023-03-17 19:27:36 +0800

    Is it right tn new b?

--- PushTest.cpp
+++ PushTest.cpp
@@ -2,9 +2,18 @@
 
 using namespace std ;
 
+
+int exAdd( int a, int b)
+{
+  return a+b ;
+}
+
 int main()
 {
     cout << "Please work! " << endl ;
     cout << "I add an new feature here." << endl ;
+    int ans = 0 ;
+    ans = exAdd(10, 588) ;
+    cout << ans << endl ;
     return 0 ;
 }
